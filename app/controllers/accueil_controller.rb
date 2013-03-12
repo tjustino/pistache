415e69478500dc4002abe7b3035b25f0d068a825
tomj <thomas.justino@gmail.com>
2013-03-12 16:42:17 +0100

    Home correction

--- app/controllers/accueil_controller.rb
+++ app/controllers/accueil_controller.rb
@@ -2,6 +2,6 @@ class AccueilController < ApplicationController
   def index
     p = Projet.arel_table
     t = Tache.arel_table
-    @infos = Projet.where(p[:pause].eq(false).and((p[:date_fin].eq(nil)).or(p[:date_fin].eq("")))).includes(:taches).where((t[:date_fin].eq(nil)).or(t[:date_fin].eq(nil)))
+    @infos = Projet.where(p[:pause].eq(false).and((p[:date_fin].eq(nil)).or(p[:date_fin].eq(nil)))).includes(:taches).where((t[:date_fin].eq(nil)).or(t[:date_fin].eq(nil)))
   end
 end
